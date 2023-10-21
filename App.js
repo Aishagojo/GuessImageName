915163b428903e6a7cc886af34ed14f543b4e694
aishaomaa <124359762+Aishagojo@users.noreply.github.com>
2023-10-21 20:33:08 +0300

    App.js

--- App.js
+++ App.js
@@ -1,48 +1,10 @@
-const images = [
-    { name: 'titanic', src: './oceann.jpg' },
-    { name: 'pyramid', src: './pyramid.jpg' },
-    { name: 'light Yagami', src: './light Yagami.jpg' },
-    { name: 'bermuda triangle', src: './bermuda triangle.jpg' },
-    { name: 'jeffrey Dahmer', src: './jeffrey Dahmer.jpg' }
-];
-
-let score = 0;
-let currentIndex = 0;
-let timer;
-let gameStarted = false;
-
-function displayImage(index) {
-    const displayedImage = document.getElementById('displayed-image');
-    displayedImage.src = images[index].src;
-    displayedImage.alt = images[index].name;
-}
-
-function startTimer() {
-    let seconds = 15;
-    timer = setInterval(() => {
-        document.getElementById('timer').textContent = `Time left: ${seconds}s`;
-        seconds--;
-
-        if (seconds < 0) {
-            clearInterval(timer);
-            currentIndex++;
-            if (currentIndex < images.length) {
-                displayImage(currentIndex);
-                startTimer();
-            } else {
-                endGame();
-            }
-        }
-    }, 900);
-}
-
 function CheckAnswer() {
     const userGuess = document.getElementById('userInput').value.toLowerCase();
     const correctAnswer = images[currentIndex].name.toLowerCase();
 
     if (userGuess === correctAnswer) {
         score++;
-        document.getElementById('score').textContent = `Score: ${score}`;
+        document.getElementById('score').textContent = score;
     }
 
     clearInterval(timer);
@@ -57,27 +19,3 @@ function CheckAnswer() {
 
     document.getElementById('userInput').value = '';
 }
-
-function endGame() {
-    const scoreButton = document.getElementById('startButton');
-    scoreButton.textContent = `Final Score: ${score}`;
-    score = 0;
-    gameStarted = false;
-    // Reset other game variables and elements as needed
-}
-
-// Get references to buttons
-const startButton = document.getElementById('startButton');
-const submitButton = document.getElementById('submitbtn');
-
-// Add click event listener to start button
-startButton.addEventListener('click', () => {
-    if (!gameStarted) {
-        gameStarted = true;
-        displayImage(currentIndex);
-        startTimer();
-    }
-});
-
-// Add click event listener to submit button
-submitButton.addEventListener('click', CheckAnswer);
